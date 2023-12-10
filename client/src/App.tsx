fc217d1d8ec5a77705390a7c294ca7b86f72a007
Maksym Kovpak <kovpak.maksym@gmail.com>
2023-12-10 16:45:58 +0200

    Created custom dropdown component. Added navbar to the header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,12 @@
-import { Layout } from 'antd';
 import PageHeader from './components/page-header/PageHeader.tsx';
-import { Content } from 'antd/es/layout/layout';
 import { Outlet } from 'react-router-dom';
 
 const App = () => {
   return (
-    <Layout>
+    <>
       <PageHeader />
-
-      <Content>
-        <Outlet />
-      </Content>
-    </Layout>
+      <Outlet />
+    </>
   );
 };
 
--- client/src/components/page-header/PageHeader.tsx
+++ client/src/components/page-header/PageHeader.tsx
@@ -1,12 +1,44 @@
-import { Header } from 'antd/es/layout/layout';
 import logo from '../../assets/img/logo.svg';
+import { MyDropdown } from '../ui/dropdown/MyDropdown';
 import './PageHeader.scss';
+import type { MenuProps } from 'antd';
+
+const items: MenuProps['items'] = [
+  {
+    key: '1',
+    label: <a href="">Men</a>,
+  },
+  {
+    key: '2',
+    label: <a href="">Women</a>,
+  },
+];
 
 const PageHeader = () => {
   return (
-    <Header>
-      <img src={logo} alt="Shop.co" />
-    </Header>
+    <header>
+      <img src={logo} alt="SHOP.CO" />
+
+      <nav className="navbar">
+        <ul>
+          <li>
+            <MyDropdown items={items}>Shop</MyDropdown>
+          </li>
+
+          <li>
+            <a href="">On Sale</a>
+          </li>
+
+          <li>
+            <a href="">New Arrivals</a>
+          </li>
+
+          <li>
+            <a href="">Brands</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
