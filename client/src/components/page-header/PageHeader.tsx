f1f7db567408c07d5a7f6165328d095bbbfeea9a
Maksym Kovpak <kovpak.maksym@gmail.com>
2023-12-10 17:48:48 +0200

    Added header config

--- client/src/components/page-header/PageHeader.tsx
+++ client/src/components/page-header/PageHeader.tsx
@@ -1,41 +1,68 @@
+import { useTranslation } from 'react-i18next';
 import logo from '../../assets/img/logo.svg';
 import { MyDropdown } from '../ui/dropdown/MyDropdown';
 import './PageHeader.scss';
 import type { MenuProps } from 'antd';
+import { useMemo } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
-const items: MenuProps['items'] = [
-  {
-    key: '1',
-    label: <a href="">Men</a>,
-  },
-  {
-    key: '2',
-    label: <a href="">Women</a>,
-  },
-];
+export type HeaderConfig = Array<{
+  label: string;
+  link?: string;
+  items?: MenuProps['items'];
+}>;
 
 const PageHeader = () => {
+  const { t } = useTranslation();
+
+  const config: HeaderConfig = useMemo(
+    () => [
+      {
+        label: t('SHOP'),
+        items: [
+          {
+            key: '1',
+            label: <a href="">{t('SHOP_MEN')}</a>,
+          },
+          {
+            key: '2',
+            label: <a href="">{t('SHOP_WOMEN')}</a>,
+          },
+        ],
+      },
+      {
+        label: t('ON_SALES'),
+        link: '',
+      },
+      {
+        label: t('NEW_ARRIVALS'),
+        link: '',
+      },
+      {
+        label: t('BRANDS'),
+        link: '',
+      },
+    ],
+    [t]
+  );
+
   return (
     <header>
       <img src={logo} alt="SHOP.CO" />
 
       <nav className="navbar">
         <ul>
-          <li>
-            <MyDropdown items={items}>Shop</MyDropdown>
-          </li>
-
-          <li>
-            <a href="">On Sale</a>
-          </li>
-
-          <li>
-            <a href="">New Arrivals</a>
-          </li>
-
-          <li>
-            <a href="">Brands</a>
-          </li>
+          {config.map((item) =>
+            item.items === undefined ? (
+              <li key={uuidv4()}>
+                <a href={item.link ?? '/'}>{item.label}</a>
+              </li>
+            ) : (
+              <li key={uuidv4()}>
+                <MyDropdown items={item.items}>{item.label}</MyDropdown>
+              </li>
+            )
+          )}
         </ul>
       </nav>
     </header>
