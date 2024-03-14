d2570c38ac3fe95a4842adbcdcde1519553bef9d
Maksym Kovpak <kovpak.maksym@datawiz.io>
2024-03-14 13:11:25 +0200

    Added handling error message from search params

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,43 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useSearchParams } from 'react-router-dom';
 import { useUnit } from 'effector-react';
 import { useEffect } from 'react';
+import { message } from 'antd';
 import PageHeader from '@/components/layout/PageHeader';
 import Loading from '@/components/layout/Loading';
 import { fetchUserProfileFx } from '@/stores/user.store';
 
 const App = () => {
   const fetchAuthorizedUser = useUnit(fetchUserProfileFx);
+  const [params, setParams] = useSearchParams();
 
   useEffect(() => {
     fetchAuthorizedUser();
   }, [fetchAuthorizedUser]);
+
+  // Handling an error coming from search params
+  useEffect(() => {
+    const errorMessage = params.get('error');
+    const key = 'params-error';
+
+    if (errorMessage) {
+      message.open({
+        key,
+        type: 'error',
+        content: errorMessage,
+        duration: 2,
+        onClose: () => {
+          if (params.has('error', errorMessage)) {
+            setParams((prev) => {
+              prev.delete('error');
+              return prev;
+            });
+          }
+        },
+      });
+    }
+
+    return () => message.destroy(key);
+  }, [params, setParams]);
 
   return (
     <Loading>
