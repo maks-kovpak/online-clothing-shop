a54ac4c1f680524c403c37690a0e396e8ecb0ab2
Maksym Kovpak <kovpak.maksym@datawiz.io>
2024-03-22 16:58:36 +0200

    Added clothing types to the navbar

--- client/src/lib/hooks/useNavbarConfig.tsx
+++ client/src/lib/hooks/useNavbarConfig.tsx
@@ -1,9 +1,12 @@
-import { useCallback, useMemo } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { v4 as uuidv4 } from 'uuid';
 import type { MenuProps } from 'antd';
 import { NavLink } from 'react-router-dom';
 import paths from '@/lib/paths';
+import type { IClothingType } from '@server/models/ClothingTypes';
+import { Gender } from '@server/lib/enums';
+import ClothingTypesApi from '@/lib/api/clothingTypes';
 
 export type NavbarConfig = Array<{
   label: string;
@@ -11,26 +14,50 @@ export type NavbarConfig = Array<{
   children?: NavbarConfig;
 }>;
 
+const getClothingTypesItems = (types: IClothingType[], baseUrl: string, gender: Gender) => {
+  return types
+    .filter((type) => type.gender === gender || type.gender === Gender.UNISEX)
+    .map((item) => ({
+      label: item.name,
+      link: baseUrl + `?type=${item.name}`,
+    }));
+};
+
 /**
  * The `useNavbarConfig` hook generates a configuration for a navigation menu.
  * @returns The translated and modified navbar config.
  */
 const useNavbarConfig = (): MenuProps['items'] => {
   const { t } = useTranslation();
+  const [clothingTypes, setClothingTypes] = useState<IClothingType[]>([]);
+
+  useEffect(() => {
+    ClothingTypesApi.getAll().then((response) => {
+      setClothingTypes(response.data);
+    });
+  }, []);
 
   const initialConfig: NavbarConfig = useMemo(
     () => [
       {
         label: 'SHOP',
         children: [
-          { label: 'SHOP_MEN', link: paths.shopMen },
-          { label: 'SHOP_WOMEN', link: paths.shopWomen },
+          {
+            label: 'SHOP_MEN',
+            link: paths.shopMen,
+            children: getClothingTypesItems(clothingTypes, paths.shopMen, Gender.MAN),
+          },
+          {
+            label: 'SHOP_WOMEN',
+            link: paths.shopWomen,
+            children: getClothingTypesItems(clothingTypes, paths.shopWomen, Gender.WOMAN),
+          },
         ],
       },
       { label: 'ON_SALES', link: paths.sales },
       { label: 'NEW_ARRIVALS', link: paths.newArrivals },
     ],
-    []
+    [clothingTypes]
   );
 
   const modify = useCallback(
