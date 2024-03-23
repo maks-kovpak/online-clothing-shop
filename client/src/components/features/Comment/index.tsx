f7151a7b56c1db0a523802a0654726c0005a72c5
Maksym Kovpak <kovpak.maksym@gmail.com>
2024-03-23 16:20:08 +0200

    Added comments styles

--- client/src/components/features/Comment/index.tsx
+++ client/src/components/features/Comment/index.tsx
@@ -1,13 +1,21 @@
 import type { FC } from 'react';
 import type { FullComment } from '@server/lib/types/models';
-import { Rate } from 'antd';
+import { Flex, Rate } from 'antd';
+
+import ApprovedIcon from '@/assets/icons/approved.svg?react';
+import './index.scss';
 
 const Comment: FC<{ comment: FullComment }> = ({ comment }) => {
   return (
     <div className="comment">
       <Rate defaultValue={comment.rating} disabled />
-      <h4>{comment.author}</h4>
-      <p>{comment.text}</p>
+
+      <Flex gap="0.375rem">
+        <h4 className="secondary">{comment.author}</h4>
+        <ApprovedIcon />
+      </Flex>
+
+      <p className="comment-text">{comment.text}</p>
     </div>
   );
 };
--- client/src/pages/main/containers/CustomersFeedbacks/index.tsx
+++ client/src/pages/main/containers/CustomersFeedbacks/index.tsx
@@ -2,6 +2,10 @@ import CommentsApi from '@/lib/api/comments';
 import { FullComment } from '@server/lib/types/models';
 import { useEffect, useState } from 'react';
 import Comment from '@/components/features/Comment';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import './index.scss';
 
 const CustomersFeedbacks = () => {
   const [comments, setComments] = useState<FullComment[]>([]);
@@ -9,16 +13,32 @@ const CustomersFeedbacks = () => {
   useEffect(() => {
     CommentsApi.getAll({
       sortBy: 'createdAt',
-      sortOrder: 'desc',
+      sortOrder: 'descending',
     }).then((response) => setComments(response.data));
   }, []);
 
   return (
-    <div>
-      {comments.map((item) => (
-        <Comment key={item._id.toString()} comment={item} />
-      ))}
-    </div>
+    <section className="customer-feedbacks-section">
+      <Swiper
+        className="comments-carousel"
+        spaceBetween={16}
+        breakpoints={{
+          992: { slidesPerView: 3.5 },
+          576: { slidesPerView: 1.5 },
+          0: { slidesPerView: 1 },
+        }}
+        centeredSlidesBounds={true}
+        centeredSlides={true}
+        initialSlide={2}
+        loop={true}
+      >
+        {comments.map((item) => (
+          <SwiperSlide key={item._id.toString()}>
+            <Comment comment={item} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
   );
 };
 
