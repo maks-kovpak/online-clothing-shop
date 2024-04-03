1b9014b430d3abc433a7f67345b787cdf05e47d5
Maksym Kovpak <kovpak.maksym@gmail.com>
2024-04-03 20:28:06 +0300

    Added 'No Data' container

--- client/src/components/features/ProductsList/index.tsx
+++ client/src/components/features/ProductsList/index.tsx
@@ -2,6 +2,8 @@ import type { FC } from 'react';
 import type { FullProduct } from '@server/lib/types/models';
 import ProductCard from '@/components/features/ProductCard';
 import ProductCardSkeleton from '@/components/features/ProductCardSkeleton';
+import { Empty, Flex } from 'antd';
+import { useTranslation } from 'react-i18next';
 
 import './index.scss';
 
@@ -19,7 +21,16 @@ const ProductsList: FC<{
   products: FullProduct[] | undefined;
   pending?: boolean;
 }> = ({ products, pending }) => {
+  const { t } = useTranslation();
+
   if (pending || !products) return <ProductsListSkeleton />;
+
+  if (!products.length)
+    return (
+      <Flex justify="center" align="center" className="empty-container">
+        <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description={t('NO_DATA')} />
+      </Flex>
+    );
 
   return (
     <div className="products-list">
