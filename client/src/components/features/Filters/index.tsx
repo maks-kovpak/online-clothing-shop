662db54acb334f70cacdbe1de2dda617654b5161
Maksym Kovpak <kovpak.maksym@gmail.com>
2024-04-03 18:33:10 +0300

    Disabled filters sidebar when there is no products

--- client/src/components/features/Filters/index.tsx
+++ client/src/components/features/Filters/index.tsx
@@ -1,9 +1,11 @@
 import { useMemo } from 'react';
+import type { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Button, Collapse, Flex } from 'antd';
 import type { CollapseProps } from 'antd';
 import { useUnit } from 'effector-react';
 import { $filtersTouched, applyFiltersEvent } from '@/stores/filters.store';
+import clsx from 'clsx';
 
 import ChooseColor from './ChooseColor';
 import PriceSlider from './PriceSlider';
@@ -13,7 +15,7 @@ import ChooseSize from './ChooseSize';
 import FiltersIcon from '@/assets/icons/filters.svg?react';
 import './index.scss';
 
-const Filters = () => {
+const Filters: FC<{ className: string }> = ({ className }) => {
   const { t } = useTranslation();
   const [applyFilters, filtersTouched] = useUnit([applyFiltersEvent, $filtersTouched]);
 
@@ -44,7 +46,7 @@ const Filters = () => {
   );
 
   return (
-    <aside className="filters-sidebar">
+    <aside className={clsx('filters-sidebar', className)}>
       <Flex className="title" align="center" justify="space-between">
         <h2 className="secondary">{t('FILTERS')}</h2>
         <FiltersIcon />
--- client/src/pages/shop/index.tsx
+++ client/src/pages/shop/index.tsx
@@ -6,11 +6,13 @@ import { useLocation, useParams } from 'react-router-dom';
 import NotFoundPage from '@/pages/notFound';
 import ProductsList from '@/components/features/ProductsList';
 import Breadcrumbs from '@/components/features/Breadcrumbs';
+import MetaTags from '@/components/features/MetaTags';
 import { Flex } from 'antd';
 import Filters from '@/components/features/Filters';
 import { useUnit } from 'effector-react';
 import { $products, setProductsEvent, setMaxPriceEvent } from '@/stores/products.store';
 import { $appliedFilters } from '@/stores/filters.store';
+import { useTranslation } from 'react-i18next';
 
 import './index.scss';
 
@@ -22,6 +24,7 @@ type PageQueryParams = {
 const genderSlugAllowedValues = [Gender.MAN.toLowerCase(), Gender.WOMAN.toLowerCase()];
 
 const ShopPage = () => {
+  const { t } = useTranslation();
   const { gender, type } = useParams() as PageQueryParams;
   const pathname = useLocation();
 
@@ -79,16 +82,20 @@ const ShopPage = () => {
 
   return (
     <>
-      <section className="primary-section section-top-margin breadcrumbs-section">
-        <Breadcrumbs />
-      </section>
+      <MetaTags title={`SHOP.CO | ${t('SHOP_PAGE_TITLE')}`} description={t('SHOP_PAGE_DESCRIPTION')} />
 
-      <section className="primary-section">
-        <Flex gap="1.25rem">
-          <Filters />
-          <ProductsList products={products} pending={isPending} />
-        </Flex>
-      </section>
+      <main className="shop-page">
+        <section className="primary-section section-top-margin breadcrumbs-section">
+          <Breadcrumbs />
+        </section>
+
+        <section className="primary-section main-section">
+          <Flex gap="1.25rem">
+            <Filters className={!products.length ? 'disabled' : ''} />
+            <ProductsList products={products} pending={isPending} />
+          </Flex>
+        </section>
+      </main>
     </>
   );
 };
