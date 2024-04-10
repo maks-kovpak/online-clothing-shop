712f4a08f31e40ba0e477ac5c7003259e422b8fb
Maksym Kovpak <kovpak.maksym@gmail.com>
2024-04-10 20:43:53 +0300

    Added product reviews section

--- client/src/components/features/Comment/index.tsx
+++ client/src/components/features/Comment/index.tsx
@@ -2,11 +2,24 @@ import type { FC } from 'react';
 import type { FullComment } from '@server/lib/types/models';
 import { Flex } from 'antd';
 import { Rate } from '@/ui';
+import { LANGUAGES } from '@/lib/constants/languages';
+import { useTranslation } from 'react-i18next';
+import { useMemo } from 'react';
 
 import ApprovedIcon from '@/assets/icons/approved.svg?react';
 import './index.scss';
 
-const Comment: FC<{ comment: FullComment }> = ({ comment }) => {
+const Comment: FC<{
+  comment: FullComment;
+  withPublicationDate?: boolean;
+}> = ({ comment, withPublicationDate }) => {
+  const { i18n } = useTranslation();
+
+  const currentLocale = useMemo(() => {
+    const lang = LANGUAGES.find((item) => item.code === i18n.language);
+    return lang && lang.code + '-' + lang.region;
+  }, [i18n.language]);
+
   return (
     <div className="comment">
       <Rate defaultValue={comment.rating} disabled />
@@ -17,6 +30,16 @@ const Comment: FC<{ comment: FullComment }> = ({ comment }) => {
       </Flex>
 
       <p className="comment-text">{comment.text}</p>
+
+      {withPublicationDate && (
+        <span className="publication-date">
+          {new Date(comment.createdAt).toLocaleDateString(currentLocale, {
+            day: 'numeric',
+            month: 'long',
+            year: 'numeric',
+          })}
+        </span>
+      )}
     </div>
   );
 };
--- client/src/pages/productDetail/index.tsx
+++ client/src/pages/productDetail/index.tsx
@@ -1,16 +1,22 @@
 import MetaTags from '@/components/features/MetaTags';
 import ProductsApi from '@/lib/api/products';
+import CommentsApi from '@/lib/api/comments';
 import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
 import NotFoundPage from '@/pages/notFound';
 import Breadcrumbs from '@/components/features/Breadcrumbs';
-import { Flex } from 'antd';
+import Comment from '@/components/features/Comment';
+import { Button, Flex } from 'antd';
 import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { ProductImagesGallery, ProductImagesGallerySkeleton } from './containers/ProductImagesGallery';
 import ProductInfo from './containers/ProductInfo';
 
+import './index.scss';
+
 const ProductDetailPage = () => {
   const { id } = useParams();
+  const { t } = useTranslation();
   const [currentOption, setCurrentOption] = useState<number>(0);
 
   const { data: product, isPending } = useQuery({
@@ -19,6 +25,16 @@ const ProductDetailPage = () => {
       if (!id) return;
 
       const response = await ProductsApi.getOne(id);
+      return response.data;
+    },
+  });
+
+  const { data: comments } = useQuery({
+    queryKey: ['productReviews'],
+    queryFn: async () => {
+      if (!id) return;
+
+      const response = await CommentsApi.getAllForProduct(id);
       return response.data;
     },
   });
@@ -44,6 +60,21 @@ const ProductDetailPage = () => {
 
             <ProductInfo product={product} option={currentOption} setOption={setCurrentOption} pending={isPending} />
           </Flex>
+        </section>
+
+        <section className="product-reviews primary-section">
+          <Flex className="reviews-title" justify="space-between">
+            <h3 className="secondary">
+              {t('ALL_REVIEWS')}
+              {comments && <span>({comments.length})</span>}
+            </h3>
+
+            <Button type="primary">{t('WRITE_REVIEW')}</Button>
+          </Flex>
+
+          <div className="reviews-container">
+            {comments?.map((item) => <Comment key={item._id.toString()} comment={item} withPublicationDate />)}
+          </div>
         </section>
       </main>
     </>
