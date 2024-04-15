f25284ba376e2e2189f8f39d99e8abbe0c243742
Maksym Kovpak <kovpak.maksym@gmail.com>
2024-04-15 20:47:04 +0300

    Changed cart item layout

--- client/src/components/features/CartItem/index.tsx
+++ client/src/components/features/CartItem/index.tsx
@@ -1,21 +1,47 @@
 import join from 'url-join';
-import { Flex } from 'antd';
+import { Button, Flex } from 'antd';
 import { UPLOAD_URL } from '@/lib/constants';
-import Color from '../Color';
+import { InputNumber } from '@/ui';
+import { useTranslation } from 'react-i18next';
+import ProductPrice from '../ProductPrice';
 import type { FullCartItem } from '@server/lib/types/models';
 import type { FC } from 'react';
+
+import BinIcon from '@/assets/icons/bin.svg?react';
 
 import './index.scss';
 
 const CartItem: FC<{ item: FullCartItem }> = ({ item }) => {
+  const { t } = useTranslation();
+
   return (
     <Flex className="cart-item" gap="1rem">
       <img src={join(UPLOAD_URL, item.image)} alt={item.name} />
 
-      <div className="cart-item-body">
-        <h4 className="secondary">{item.name}</h4>
-        <Color value={item.color} checkable={false} />
-      </div>
+      <Flex className="cart-item-body" justify="space-between" vertical>
+        <div>
+          <Flex justify="space-between" align="center">
+            <h4 className="secondary">{item.name}</h4>
+            <Button type="link" className="delete-item-button">
+              <BinIcon />
+            </Button>
+          </Flex>
+
+          <Flex align="center" gap="0.5rem">
+            <b>{t('COLOR')}:</b>
+            <div className="item-color" style={{ backgroundColor: item.color }}></div>
+          </Flex>
+
+          <span>
+            <b>{t('SIZE')}:</b> {item.size}
+          </span>
+        </div>
+
+        <Flex justify="space-between" align="end">
+          <ProductPrice value={item.price} />
+          <InputNumber min={1} initialValue={item.count} />
+        </Flex>
+      </Flex>
     </Flex>
   );
 };
