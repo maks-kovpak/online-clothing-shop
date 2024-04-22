fe607b38c20106298bdcee302b34857e8ae418c4
Maksym Kovpak <kovpak.maksym@gmail.com>
2024-04-22 16:13:59 +0300

    Added localized currency formats to the order summary

--- client/src/pages/cart/containers/OrderSummary/index.tsx
+++ client/src/pages/cart/containers/OrderSummary/index.tsx
@@ -3,15 +3,23 @@ import { Button, Divider, Flex } from 'antd';
 import { useUnit } from 'effector-react';
 import { sum } from 'lodash';
 import { useMemo } from 'react';
+import { LANGUAGES } from '@/lib/constants/languages';
+import { useTranslation } from 'react-i18next';
 
 import './index.scss';
-import { useTranslation } from 'react-i18next';
 
 const deliveryFee = 75;
 
 const OrderSummary = () => {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
   const cart = useUnit($cart);
+
+  const currencyFormatter = useMemo(() => {
+    const lang = LANGUAGES.find((item) => item.code === i18n.language);
+    const locale = lang && lang.code + '-' + lang.region;
+
+    return new Intl.NumberFormat(locale);
+  }, [i18n.language]);
 
   const initialTotalPrice = useMemo(() => sum(cart?.map((item) => item.initialPrice * item.count)), [cart]);
   const currentTotalPrice = useMemo(() => sum(cart?.map((item) => item.price * item.count)), [cart]);
@@ -22,24 +30,24 @@ const OrderSummary = () => {
 
       <Flex className="summary-field subtotal" justify="space-between" align="center">
         <span>{t('SUBTOTAL')}</span>
-        <span>₴{initialTotalPrice}</span>
+        <span>₴{currencyFormatter.format(initialTotalPrice)}</span>
       </Flex>
 
       <Flex className="summary-field discount" justify="space-between" align="center">
         <span>{t('DISCOUNT')}</span>
-        <span>-₴{initialTotalPrice - currentTotalPrice}</span>
+        <span>-₴{currencyFormatter.format(initialTotalPrice - currentTotalPrice)}</span>
       </Flex>
 
       <Flex className="summary-field delivery-fee" justify="space-between" align="center">
         <span>{t('DELIVERY_FEE')}</span>
-        <span>₴{deliveryFee}</span>
+        <span>₴{currencyFormatter.format(deliveryFee)}</span>
       </Flex>
 
       <Divider />
 
       <Flex className="summary-field total" justify="space-between" align="center">
         <span>{t('TOTAL')}</span>
-        <span>₴{currentTotalPrice + deliveryFee}</span>
+        <span>₴{currencyFormatter.format(currentTotalPrice + deliveryFee)}</span>
       </Flex>
 
       <Button type="primary" size="large" className="complete-order-button">
